a7aa420d586e0a47f794350cc40fd33d0937316d
Ahren Swett <ahrenswett@gmail.com>
2022-06-23 10:40:12 -0700

    Made CabinetViewEvent class

--- app/src/main/java/com/ahrenswett/pillminder/ui/cabinet_view/CabinetViewEvent.kt
+++ app/src/main/java/com/ahrenswett/pillminder/ui/cabinet_view/CabinetViewEvent.kt
@@ -1,5 +1,11 @@
 package com.ahrenswett.pillminder.ui.cabinet_view
 
-class CabinetViewEvent {
+import com.ahrenswett.pillminder.domain.model.Bottle
 
+sealed class CabinetViewEvent {
+    // Add a Bottle
+    data class AddBottle (val bottle: Bottle): CabinetViewEvent()
+    // Open Menu
+    object OpenMenu : CabinetViewEvent()
+    // Expand Bottle Description
 }
--- app/src/main/java/com/ahrenswett/pillminder/ui/cabinet_view/CabinetViewScreen.kt
+++ app/src/main/java/com/ahrenswett/pillminder/ui/cabinet_view/CabinetViewScreen.kt
@@ -20,6 +20,19 @@ import com.ahrenswett.pillminder.domain.model.Bottle
 import com.ahrenswett.pillminder.util.UiEvent
 import kotlinx.coroutines.flow.collect
 
+/*TODO:
+*   - Add Floating action button to add bottles
+*       - add reminder at time of adding new bottle
+*   - Add list of bottles
+*       - clicking a bottle just expands for details
+*           - has edit option
+*       - swiping deletes
+*           - implement snackbar undo delete
+*   - Add hamburger menu
+*       - delete cabinet
+*       - edit cabinet
+*/
+
 @Composable
 fun CabinetViewScreen(
     onPopBackStack: () -> Unit,
@@ -29,19 +42,19 @@ fun CabinetViewScreen(
     val cabinet = viewModel.cabinet
     cabinet?.name?.let { Log.i( "CabinetViewScreen", it) }
 
-    LaunchedEffect(key1 = true){
-        viewModel.uiEvent.collect { event ->
-            when(event){
-                is UiEvent.PopBackStack -> onPopBackStack()
-                else -> Unit
-            }
-        }
-    }
+
+//    LaunchedEffect(key1 = true){
+//        viewModel.uiEvent.collect { event ->
+//            when(event){
+//                is UiEvent.PopBackStack -> onPopBackStack()
+//                else -> Unit
+//            }
+//        }
+//    }
 
     Scaffold (
         scaffoldState = scaffoldState,
         topBar = {
-
             TopAppBar(
                 title = {
                     if (cabinet != null) {
