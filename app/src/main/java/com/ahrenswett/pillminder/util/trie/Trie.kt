bb6d7723034d32ef5f256247aefc0b2d0e18c3c0
Ahren Swett <ahrenswett@gmail.com>
2022-06-26 22:36:14 -0700

    Implemented Trie with insert, contains and get first 5 methods.

--- app/src/main/java/com/ahrenswett/pillminder/util/trie/Trie.kt
+++ app/src/main/java/com/ahrenswett/pillminder/util/trie/Trie.kt
@@ -1,6 +1,73 @@
 package com.ahrenswett.pillminder.util.trie
 
-class Trie {
-    private val root = TrieNode(root = true, isEnd = true, children = )
-    // TODO: implement
+
+class Trie<Key> (private val root: TrieNode<Key> = TrieNode<Key>( key = null, parent = null)){
+
+   fun insert(list : List<Key>){
+       var currentNode = root
+       list.forEach{ key ->
+           if (currentNode.children.containsKey(key)){
+               currentNode = currentNode.children[key]!!
+           }
+           else currentNode.children[key] = TrieNode(key, currentNode)
+       }
+       currentNode.isEnd = true
+    }
+
+    fun contains(list : List<Key>): Boolean{
+        var currentNode = root
+        list.forEach{ key ->
+            if (currentNode.children.containsKey(key)){
+                currentNode = currentNode.children[key]!!
+            }
+            else return false
+        }
+        return true
+    }
+
+    fun getFirst5(key: Key): List<String>{
+    //        builder to build each string
+        val string = StringBuilder()
+    //        list to append the strings to
+        val listOF5 = mutableListOf<String>()
+    //        current node to traverse the trie
+        var currentNode = root
+    //        Queue to keep track of the nodes to traverse
+        val nodeQueue = mutableListOf<TrieNode<Key>>()
+
+    //        if it contains first Key it may have more.
+        if(currentNode.children.containsKey(key)){
+            string.append(key)
+
+            // store key as currentNode
+            currentNode = currentNode.children[key]!!
+            // if it is end of the word add to list
+            if (currentNode.isEnd){
+                listOF5.add(string.toString())
+            }
+            // get currentNode's list of children and add to Queue
+            for (node in currentNode.children.keys.toList()) run { ->
+                nodeQueue.add(currentNode.children[node]!!)
+            }
+            return if (nodeQueue.isEmpty()) listOF5
+                else{
+                    while (nodeQueue.isNotEmpty() && listOF5.size < 5){
+                        // get first node in queue
+                        currentNode = nodeQueue.removeAt(0)
+                        // add key to string
+                        string.append(currentNode.key)
+                        // if it is end of the word add to list
+                        if (currentNode.isEnd){
+                            listOF5.add(string.toString())
+                        }
+                        // get currentNode's list of children and add to Queue
+                        for (node in currentNode.children.keys.toList()) run { ->
+                            nodeQueue.add(currentNode.children[node]!!)
+                        }
+                    }
+                    return listOF5
+                }
+            }
+            return listOF5  // if it doesn't contain first key return empty list
+    }
 }
--- app/src/main/java/com/ahrenswett/pillminder/util/trie/TrieNode.kt
+++ app/src/main/java/com/ahrenswett/pillminder/util/trie/TrieNode.kt
@@ -1,8 +1,7 @@
 package com.ahrenswett.pillminder.util.trie
 
-import dagger.hilt.internal.definecomponent.DefineComponentNoParent
 
-class TrieNode<Key> (var key: Key, var parent: TrieNode<Key>?){
+class TrieNode<Key> (var key: Key?, var parent: TrieNode<Key>?){
     val children = mutableMapOf<Key, TrieNode<Key>>()
     var isEnd = false
 }
